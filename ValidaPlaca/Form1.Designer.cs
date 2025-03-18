3d659d72f336d943a547f682f7a98145dfacb109
jonatas-joao <jojokl@hotmail.com>
2025-03-17 21:17:01 -0300

    Adicionar arquivos de projeto.

---
+++ ValidaPlaca/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace ValidaPlaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtPlaca = new TextBox();
+            btnValidar = new Button();
+            SuspendLayout();
+            // 
+            // txtPlaca
+            // 
+            txtPlaca.Location = new Point(94, 67);
+            txtPlaca.Name = "txtPlaca";
+            txtPlaca.Size = new Size(128, 23);
+            txtPlaca.TabIndex = 0;
+            // 
+            // btnValidar
+            // 
+            btnValidar.Location = new Point(120, 111);
+            btnValidar.Name = "btnValidar";
+            btnValidar.Size = new Size(75, 23);
+            btnValidar.TabIndex = 1;
+            btnValidar.Text = "Validar";
+            btnValidar.UseVisualStyleBackColor = true;
+            btnValidar.Click += btnValidar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(345, 178);
+            Controls.Add(btnValidar);
+            Controls.Add(txtPlaca);
+            Name = "Form1";
+            Text = "Valida Placa de Carro";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtPlaca;
+        private Button btnValidar;
+    }
+}
---
+++ ValidaPlaca/Form1.cs
@@ -0,0 +1,33 @@
+namespace ValidaPlaca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            if (ValidaPlaca(txtPlaca.Text))
+                MessageBox.Show("Placa Válida");
+            else
+                MessageBox.Show("Placa Inválida");
+        }
+
+        private bool ValidaPlaca(string placa)
+        {
+            if(string.IsNullOrEmpty(placa) || placa.Length!=8)
+                return false;
+            else
+            {
+                string inicioDaPlaca = placa.Substring(0, 3);
+                string finalDaPlaca = placa.Substring(4);
+                if (placa[3]!= '-' || !inicioDaPlaca.All(char.IsLetter) || !finalDaPlaca.All(char.IsDigit))
+                    return false;
+                else
+                    return true;
+            }
+        }
+    }
+}
